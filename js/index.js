ce74e32604bdb40e2bcf6c804a493dc327992987
Jenskjr <Jenskjr@gmail.com>
2018-10-29 16:19:18 +0100

    changes

--- js/index.js
+++ js/index.js
@@ -31,15 +31,22 @@ $(document).ready(function(){
 	$("#hero_headline").text("Jens Kjær");
 
 	//Projects
+	$("#project_mapApp_header").text("Map-app eksampel");
+	$("#project_mapApp_subheader").text("React native projekt");
+	$("#project_mapApp_description").text("Mobil app til iPhone. Du kan teste app'en ved at installere Expo app, klikke på linket her og scanne QR-koden.");
 
-	$("#projects_category_header_1").text("Webshop eksempel");
-	$("#projects_category_header_2").text("To-do liste app");
-	$("#projects_category_header_3").text("Liverpool resultater API");
+	$("#project_webshop_header").text("Webshop eksempel");
+	$("#project_webshop_subheader").text("React & Redux projekt");
+	$("#project_webshop_description").text("Webshop som henter vareinformationer fra PunkAPI Beer Shop.");
 
-	$("#projects_category_subheader_1").text("React & Redux projekt");
-	$("#projects_category_subheader_2").text("React projekt");
-	$("#projects_category_subheader_3").text("MySQL & PHP projekt");
+	$("#project_todo_header").text("To-do liste app");
+	$("#project_todo_subheader").text("React projekt");
+	$("#project_todo_description").text("Task manager hvormed man kan liste og sortere sine to-do opgaver.");
 
+	$("#project_liverpool_stats_api_header").text("API eksempel");
+	$("#project_liverpool_stats_api_subheader").text("MySQL & PHP projekt");
+	$("#project_liverpool_stats_api_description").text("Et Api eksempel, som udstiller Liverpool FC's Premier League resultater.");
+	$("#project_liverpool_stats_api_link").text("Link");
 	$(".projects_github").text("Github");
 
 	//CV
@@ -48,11 +55,10 @@ $(document).ready(function(){
 	$("#cv_phone").text("61 65 47 76");
 	
 	$("#cv_programing_headline").text("Programmering");
-	$("#cv_programing").text("JavaScript, PHP, SQL, HTML, CSS");
+	$("#cv_programing").text("JavaScript, React, Redux, PHP, SQL, HTML, CSS");
 
 	$("#cv_course_headline").text("Kurser");
 	$("#cv_course_1").text("Front-end med React og Redux");
-	$("#cv_course_2").text("Journalistikkens grundtrin");
 
 	$("#cv_language_headline").text("Sprog");
 	$("#cv_language").text("Dansk, tysk og engelsk");	
