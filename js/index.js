ccaa72f2f500de42b0510eb1b382d57cbd52fbe0
Jenskjr <Jenskjr@gmail.com>
2018-11-04 21:08:36 +0100

    Changes

--- js/index.js
+++ js/index.js
@@ -2,36 +2,13 @@
 
 $(document).ready(function(){
 
-	const home_position = $("body").offset().top;
-	const projects_position = $('#projects').offset().top;
-	const cv_position = $("#cv").offset().top;	
-
-	//Scroll css functionality
-	$(window).scroll(function() {
-		if ($(window).scrollTop() >= (projects_position-200) ) {
-			$("a.current").removeClass("current");
-		 	$("#projectsButton").addClass("current");
-		}
-		
-		
-		if ($(window).scrollTop() >= (cv_position-100) ) {
-			$("a.current").removeClass("current");
-		 	$("#cvButton").addClass("current");
-		}	
-
-		if ($(window).scrollTop() < (home_position+50) ) {
-			$("a.current").removeClass("current");
-		 	$("#homeButton").addClass("current");
-		}
-	});
-
 	//Text content	
 	//Hero
 	$("#hero_tagline").text("Frontend-udvikling");
 	$("#hero_headline").text("Jens Kjær");
 
 	//Projects
-	$("#project_mapApp_header").text("Map-app eksampel");
+	$("#project_mapApp_header").text("Kort-app eksampel");
 	$("#project_mapApp_subheader").text("React native projekt");
 	$("#project_mapApp_description").text("Mobil app til iPhone. Du kan teste app'en ved at installere Expo app, klikke på linket her og scanne QR-koden.");
 
@@ -39,14 +16,14 @@ $(document).ready(function(){
 	$("#project_webshop_subheader").text("React & Redux projekt");
 	$("#project_webshop_description").text("Webshop som henter vareinformationer fra PunkAPI Beer Shop.");
 
-	$("#project_todo_header").text("To-do liste app");
+	$("#project_todo_header").text("Task manager eksempel");
 	$("#project_todo_subheader").text("React projekt");
 	$("#project_todo_description").text("Task manager hvormed man kan liste og sortere sine to-do opgaver.");
 
 	$("#project_liverpool_stats_api_header").text("API eksempel");
-	$("#project_liverpool_stats_api_subheader").text("MySQL & PHP projekt");
+	$("#project_liverpool_stats_api_subheader").text("MySQL, PHP & Chart.js projekt");
 	$("#project_liverpool_stats_api_description").text("Et Api eksempel, som udstiller Liverpool FC's Premier League resultater.");
-	$("#project_liverpool_stats_api_link").text("Link");
+	$("#project_liverpool_stats_api_link").text("API");
 	$(".projects_github").text("Github");
 
 	//CV
@@ -79,4 +56,101 @@ $(document).ready(function(){
 	$("#footer_copyright").text("");
 	$("#footer-tagline").text("Frontend-udvikling");
 	$("#footer_url").text("Jenskjr.dk");
-});
+
+
+	//Scroll css functionality
+	const home_position = $("body").offset().top;
+	const projects_position = $('#projects').offset().top;
+	const cv_position = $("#cv").offset().top;	
+
+	$(window).scroll(function() {
+		if ($(window).scrollTop() >= (projects_position-200) ) {
+			$("a.current").removeClass("current");
+		 	$("#projectsButton").addClass("current");
+		}
+		
+		
+		if ($(window).scrollTop() >= (cv_position-100) ) {
+			$("a.current").removeClass("current");
+		 	$("#cvButton").addClass("current");
+		}	
+
+		if ($(window).scrollTop() < (home_position+50) ) {
+			$("a.current").removeClass("current");
+		 	$("#homeButton").addClass("current");
+		}
+	});
+
+	//Chart
+	const pointsChart = (year, points) => {
+		var ctx = $("#chartNumPoints");
+		var chart = new Chart(ctx, {
+	    // The type of chart we want to create
+	    type: 'bar',
+	    // The data for our dataset
+	    data: {
+	        labels: year,
+	        datasets: [{
+	            label: "Number of Points",
+	            borderColor: '#aaa',
+	            data: points,
+	        }]
+	    },
+    	// Configuration options go here
+    	options: {}
+		});
+	}
+
+	const positionsChart = (year, position) => {
+		var ctx = $("#chartPositions");
+		//let position = [7, 6, 8, 7, 2, 6, 8, 4, 4];
+		var chart = new Chart(ctx, {
+	    // The type of chart we want to create
+	    type: 'line',
+	    // The data for our dataset
+	    data: {
+	        labels: year,
+	        datasets: [{
+	            label: "Premier League position",
+	            borderColor: '#aaa',
+	            data: position,
+	        }]
+	    },
+
+	    // Configuration options go here
+	    options: {
+	  		scales: {
+	    		yAxes: [{
+	      			ticks: {
+	        			reverse: true,
+	        			min: 2, 
+	      			}
+	    		}]
+	  		}
+		}
+
+		});
+	}
+
+	//Stats data and call to charts
+	liverpoolStatsDataChart = () => {
+		const api_url = "http://jenskjr.dk/liverpool_stats/api.php";
+		fetch(api_url)
+		.then(response => 
+			response.json()
+		)	
+		.then((data) => {
+	        let year = data.results.map(obj => obj.year);
+	        let points = data.results.map(obj =>  obj.points);
+	        let position = data.results.map(obj =>  obj.position);
+	        pointsChart(year, points);
+	        positionsChart(year, position);
+	    })
+		.catch(error => {
+			console.log(error);
+		});
+	}
+	
+	liverpoolStatsDataChart();
+
+}); //document ready
